30123c17d48819f4620577cf832546d6745b5999
Kulob <mirvonkhugobakhsh@gmail.com>
2023-08-30 07:32:08 +0300

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,17 +1,14 @@
-import React, { useState } from 'react'
-import './Portfolio.scss'
-import IMG1  from "../../assets/portfolio1.png";
-import IMG_2  from "../../assets/pizza12.png";
-import  IMG2  from "../../assets/portfolio2.png";
-import  IMG3  from "../../assets/portfolio3.png";
-import  IMG4  from "../../assets/portfolio4.jpg";
-import  IMG5  from "../../assets/portfolio5.png";
-import  IMG6  from "../../assets/portfolio6.jpg";
-import { AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineCloseCircle} from "react-icons/ai";
+import React, { useState } from 'react';
+import './Portfolio.scss';
+import IMG1 from '../../assets/portfolio1.png';
+import IMG_2 from '../../assets/pizza12.png';
+import IMG2 from '../../assets/portfolio2.png';
+import IMG3 from '../../assets/portfolio3.png';
+import IMG4 from '../../assets/portfolio4.png';
+import IMG5 from '../../assets/portfolio5.png';
+import IMG6 from '../../assets/portfolio6.jpg';
+import { AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineCloseCircle } from 'react-icons/ai';
 import Slider from 'infinite-react-carousel';
-
-
-
 
 const Portfolio = () => {
   const [openModel, setOpenModel] = useState(false);
@@ -26,14 +23,14 @@ const Portfolio = () => {
       id: 1,
       image: IMG1,
       image2: IMG_2,
-      title:'React Pizza',
+      title: 'React Pizza',
       github: 'https://github.com/Kulob/React-Pizza',
       demo: 'https://dribbble.com/shots/19773335-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps',
     },
     {
       id: 2,
       image: IMG2,
-      title:'React Sneakers',
+      title: 'React Sneakers',
       github: 'https://github.com/Kulob',
       // demo: 'https://dribbble.com/shots/19773335-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
     },
@@ -72,30 +69,29 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {
-          data.map((photo, i) => {
-            return (
-              <article key={photo.id} className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={photo.image} alt="portfolio image"/>
-          </div>
-          <h3>{photo.title}</h3>
-          <div className="portfolio__item-cta">
-            <a href={photo.github} className="btn" target='_blank'>Github</a>
-            <button onClick={() => handleOpen(i)} className="btn btn-primary">Live Demo</button>  
-          </div>         
-        </article>
-            )})
-        }
-         {
-          openModel && 
-          <div className='slider'>
-          <AiOutlineCloseCircle onClick={() => setOpenModel(false)} className='close'/>
-            <Slider className='sliderWrapper'>
+        {data.map((photo, i) => {
+          return (
+            <article key={photo.id} className="portfolio__item">
+              <div onClick={() => handleOpen(i)} className="portfolio__item-image">
+                <img src={photo.image} alt="portfolio image" />
+              </div>
+              <h3>{photo.title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={photo.github} className="btn" target="_blank">
+                  Github
+                </a>
+                {/* <a href={photo.demo} className="btn btn-primary">
+                  Live Demo
+                </a> */}
+              </div>
+            </article>
+          );
+        })}
+        {openModel && (
+          <div className="slider">
+            <AiOutlineCloseCircle onClick={() => setOpenModel(false)} className="close" />
 
-          <img src={data[slideNumber].image} alt="" className="sliderImg" />
-          <img src={data[slideNumber].image2} alt="" className="sliderImg" />
-            </Slider>
+            <img src={data[slideNumber].image} alt="" className="sliderImg" />
           </div>
         )}
       </div>
