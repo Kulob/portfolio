76c8162ea6b91e1a6c370eb9cc3744a6369cc207
Khudobakhsh <mirvonkhugobakhsh@gmail.com>
2025-01-07 15:05:26 +0500

    information update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import './About.scss';
-import { FaAward } from 'react-icons/fa';
-import { FiUsers } from 'react-icons/fi';
-import { AiOutlineFolderOpen } from 'react-icons/ai';
-import AboutImage from '../../assets/meAbout.jpg';
+import React from "react";
+import "./About.scss";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { AiOutlineFolderOpen } from "react-icons/ai";
+import AboutImage from "../../assets/meAbout.jpg";
 
 const About = () => {
   return (
@@ -20,36 +20,39 @@ const About = () => {
 
         <div className="about__content">
           <div className="about__cards">
-            {/* <article className="about__card">
+            <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>1+ Years Working</small>
-            </article> */}
-            {/* <article className="about__card">
+              <small>2+ Years Working</small>
+            </article>
+            <article className="about__card">
               <FiUsers className="about__icon" />
-              <h5>Experience</h5>
-              <small>200+ Worldwide</small>
-            </article> */}
-            {/* <article className="about__card">
+              <h5>Organizations</h5>
+              <small>2+ Worldwide</small>
+            </article>
+            <article className="about__card">
               <AiOutlineFolderOpen className="about__icon" />
-              <h5>Experience</h5>
+              <h5>Projects</h5>
               <small>20+ Completed</small>
-            </article> */}
+            </article>
           </div>
           <p>
-            Здравствуйте! Я - фронтенд-разработчик с более чем 1,5-летним опытом работы в этой
-            области. Я специализируюсь на создании веб-сайтов и приложений, используя HTML, CSS,
-            JavaScript и TypeScript. Я имею опыт работы с фреймворк, такими как React и умею
-            работать с библиотеками, такими как Tailwind и MUI.
-          </p>
-          <p>
-            Я также обладаю навыками работы с базами данных, такими как MySQL и MongoDB, и имею опыт
-            работы с системами контроля версий, такими как Git. Я всегда стремлюсь к созданию
-            качественного и удобного для пользователей интерфейса, а также к оптимизации
-            производительности веб-приложений.
-            <br /> Мои сильные стороны включают в себя разработку адаптивных веб-сайтов, создание
-            пользовательских интерфейсов, оптимизацию производительности, а также работу с API и
-            интеграцию с другими сервисами.
+            Hello, I am a frontend developer with over 2 years of experience in
+            creating modern and functional web interfaces. My goal is to develop
+            user-friendly, performant and aesthetically pleasing products that
+            fulfil user needs and business goals. I specialise in building
+            components and applications using React, Redux, Zustand and other
+            modern tools. I am confident with TypeScript, which helps me write
+            robust and scalable code.
+            <br />
+            <b>Frontend development:</b> React, Redux, Zustand, Redux Toolkit,
+            JavaScript (ES6+), TypeScript, Next.js, React Query, React Hook
+            Forms. <br />
+            <b>Backend:</b> Express.js, Node.js, Git, Nest.js <br />
+            <b>Databases:</b> MongoDB <br />
+            <b>Layout:</b> HTML5, CSS3, SCSS/SASS, Tailwind <br />
+            <b> UI libraries:</b> MUI, Zod, Ant Design, Chakra UI Cross-browser
+            and adaptive layout <br />
           </p>
           <a href="#contact" className="btn btn-primary">
             Let's Talk
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,21 +1,15 @@
 import React, { useState } from 'react';
 import './Portfolio.scss';
 import IMG1 from '../../assets/portfolio1.png';
-import IMG_2 from '../../assets/pizza12.png';
-import IMG2 from '../../assets/portfolio2.png';
-import IMG3 from '../../assets/portfolio3.png';
-import IMG4 from '../../assets/portfolio4.png';
+import IMG2 from '../../assets/zypl website.jpg';
+import IMG3 from '../../assets/photo_2025-01-07 14.50.44.jpeg';
+import IMG4 from '../../assets/aion.jpeg';
 import IMG5 from '../../assets/portfolio5.png';
 import IMG6 from '../../assets/portfolio6.png';
-import { AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineCloseCircle } from 'react-icons/ai';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 const Portfolio = () => {
   const [openModel, setOpenModel] = useState(false);
-  const [slideNumber, setSlideNumber] = useState(0);
-  const handleOpen = (i) => {
-    setSlideNumber(i);
-    setOpenModel(true);
-  };
 
   const data = [
     {
@@ -23,42 +17,42 @@ const Portfolio = () => {
       image: IMG1,
       title: 'React Pizza',
       github: 'https://github.com/Kulob/React-Pizza',
-      demo: 'https://dribbble.com/shots/19773335-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps',
+      demo: 'https://github.com/Kulob',
     },
     {
       id: 2,
       image: IMG2,
-      title: 'React Sneakers',
+      title: 'Zypl.ai',
       github: 'https://github.com/Kulob',
-      // demo: 'https://dribbble.com/shots/19773335-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+      demo: 'https://zypl.ai/en'
     },
     {
       id: 3,
       image: IMG3,
-      title: 'React Lamastore',
+      title: 'Verido.ai Dashboard',
       github: 'https://github.com/Kulob',
-      // demo: 'https://dribbble.com/shots/19314386-Visualization-of-global-data-on-the-interactive-map'
+      demo: 'https://verido-aidoc.switzerlandnorth.cloudapp.azure.com/'
     },
     {
       id: 4,
       image: IMG4,
-      title: 'React Planes',
+      title: 'Aion.tj',
       github: 'https://github.com/Kulob/Planes.git',
-      // demo: 'https://dribbble.com/shots/19773335-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+      demo: 'https://aion.tj/'
     },
     {
       id: 5,
       image: IMG5,
       title: 'React Booking',
       github: 'https://github.com/Kulob/mern-hotel.git',
-      // demo: 'https://dribbble.com/shots/19773335-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+      demo: 'https://mern-hotel.vercel.app/'
     },
     {
       id: 6,
       image: IMG6,
       title: 'React Blog',
       github: 'https://github.com/Kulob/mern-fullstack.git',
-      // demo: 'https://dribbble.com/shots/19773335-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+      demo: 'https://github.com/Kulob'
     },
   ];
   return (
@@ -70,7 +64,7 @@ const Portfolio = () => {
         {data.map((photo, i) => {
           return (
             <article key={photo.id} className="portfolio__item">
-              <div onClick={() => handleOpen(i)} className="portfolio__item-image">
+              <div  className="portfolio__item-image">
                 <img src={photo.image} alt="portfolio image" />
               </div>
               <h3>{photo.title}</h3>
@@ -78,20 +72,13 @@ const Portfolio = () => {
                 <a href={photo.github} className="btn" target="_blank">
                   Github
                 </a>
-                {/* <a href={photo.demo} className="btn btn-primary">
+                <a href={photo.demo} className="btn btn-primary">
                   Live Demo
-                </a> */}
+                </a>
               </div>
             </article>
           );
         })}
-        {openModel && (
-          <div className="slider">
-            <AiOutlineCloseCircle onClick={() => setOpenModel(false)} className="close" />
-
-            <img src={data[slideNumber].image} alt="" className="sliderImg" />
-          </div>
-        )}
       </div>
     </section>
   );
