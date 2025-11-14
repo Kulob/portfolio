251dcdbf950bbaee97feb876b33e7fc89dfafee3
Khudobakhsh <mirvonkhugobakhsh@gmail.com>
2025-11-14 16:53:31 +0500

    TanStack

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -66,6 +66,13 @@ const Experience = () => {
                 <small className="text-light">Experienced</small>
               </div>
             </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>TanStack</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
           </div>
         </div>
         <div className="experience__backend">
